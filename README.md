f92131a97f6a3f80d0c8e67cd255586f99b2e83d
Ogan Özkul <oganozkul@gmail.com>
2022-01-04 23:30:26 +0100

    adding the non-metric method support

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ryuryuBMI
 A simple Python script that will tell you your health state by using your BMI
 
-# WARNING
+## WARNING
 The BMI is only used to <strong>speculate</strong> your overall health.
 Thus not counting all the health-related activities (like Workout, Bodybuilding and other...)
 
@@ -11,8 +11,11 @@ It's easy!
 All you need is a Command Interpreter (Linux's Terminal or the Windows's CMD) and then type the following line
 
 ```
-python ryuryuBMI.py [your weight in kg] [your height by m]
+python ryuryuBMI.py [your weight in kg] [your height by m] [/lbs]
 ```
+
+If you want to use the non-metric formula (weight in lbs). You have to add <i>/lbs</i> at the end of the command line
+Since the metric method (weight in kg) would be used by default without it!
 
 ## <i>Hey! on what does your BMI measure is based on :angry_emoji</i>
 It's based on this chart (found on the following link : https://cansa.org.za/files/2020/02/BMI-table-for-LRAT.jpg)
@@ -23,7 +26,7 @@ It's based on this chart (found on the following link : https://cansa.org.za/fil
 
 ## Examples
 
-(mine :P)
+mine :P
 
 ```
 python ryuryuBMI.py 75 1.79
@@ -35,4 +38,6 @@ python ryuryuBMI 200 1.65
 ```
 
 ## TODO
-* Adding the <i>british way to check BMI</i> support
+* Showing a sort of graphical result by using GUI?
+* Triggering a GUI version of it if the user doesn't put any arguments (just by using <i>python ryuryuBMI.py</i>)?
+* Making a <i>decent</i> documentation for it...
--- ryuryuBMI.py
+++ ryuryuBMI.py
@@ -1,14 +1,16 @@
 # for the command line arguments
+# sys.argv
 import sys
 
-# pow
+# for the whole mathematics
+# math.pow
 import math
 
 # the final result
 BMI = 0
 
 # the helping text
-HELPsyntax = "Here's the syntax\n\npython ryuryuBMI.py [weight in kg] [height in m]\n\n"
+HELPsyntax = "Here's the syntax\n\n[METRIC METHOD]\npython ryuryuBMI.py [weight in kg] [height in m]\n\n[NON-METRIC METHOD]\npython ryuryuBMI.py [weight in lbs] [height in m] /lbs"
 
 # if the user type 'ryuryuBMI.py /?' -> show the help
 # otherwise -> if there's more than 3 arguments (counting the ryuryuBMI.py) -> go make the calculations
@@ -17,10 +19,14 @@ try:
     if (sys.argv[1] == '/?'):
         print(HELPsyntax)
     else:
-        if len(sys.argv) == 3:
+        if (len(sys.argv)) >= 3:
             try:
-                # applying the kg formula
-                BMI = math.trunc(float(sys.argv[1]) / math.pow(float(sys.argv[2]), 2))
+                if (len(sys.argv) == 4 and sys.argv[3] == '/lbs'):
+                    # applying the non-metric formula
+                    BMI = math.trunc((703 * float(sys.argv[1])) / math.pow(float(sys.argv[2]), 2))
+                else:
+                    # applying the metric formula
+                    BMI = math.trunc(float(sys.argv[1]) / math.pow(float(sys.argv[2]), 2))
 
                 # checking the result based on the BMI table
                 # https://cansa.org.za/files/2020/02/BMI-table-for-LRAT.jpg
@@ -37,10 +43,13 @@ try:
                     print("I think you will die soon!")
                 else:
                     print("Something went wrong...")
-            except:
-                print(HELPsyntax)    
+            except Exception as E:
+                # printing the helping text
+                # print(E)
+                print(HELPsyntax)
         else:
             # printing the helping text
             print(HELPsyntax)
-except:
+except Exception as E :
+    # print(E)
     print(HELPsyntax)
